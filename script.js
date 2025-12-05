5de69824ba5df862305bf70805142a73839d570f
SnowCanCode <parkerking0733@gmail.com>
2025-12-05 18:09:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const toggle = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+}
+
+// Toggle click
+toggle.addEventListener("click", () => {
+  let current = document.documentElement.getAttribute("data-theme");
+
+  if (current === "dark") {
+    document.documentElement.removeAttribute("data-theme");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  }
+});
